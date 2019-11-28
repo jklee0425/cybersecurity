611b4a585a02559f5e3f3f7d4f3236217df95ec3
cobarrr <56806900+cobarrr@users.noreply.github.com>
2019-11-28 14:54:36 -0800

    sql connected

---
+++ finalJar/AccessControl.java
@@ -0,0 +1,105 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ErikD
+ */
+public class AccessControl {
+
+    private static String[] roleArray = {"CUSTODIAN","SALESPERSON","WAREHOUSE"};
+    private static boolean[] readWrite = new boolean[2];
+    public static String loginDatabase = "jdbc:mysql://35.247.4.229:3306/LoginSchema";
+    public static String accountDatabase = "jdbc:mysql://35.247.4.229:3306/Accounts";
+    public static String inventoryDatabase = "jdbc:mysql://35.247.4.229:3306/Inventory";
+    /*
+    public static boolean writePrivelege(String userRole){
+        //only TOP_ADMIN role can write to the server
+        if(userRole != SUPERUSER){
+            return false;
+        }
+        return true;
+    }
+    */
+    /*
+    public static boolean readPrivelege(String userRole){
+    //employee from TOP ADMIN to EMPLOYE can read
+    if(userRole != SUPERUSER || userRole != EMPLOYEE){
+        return false;
+    }
+    return true;
+    }
+    */
+    public static String showCommands(){
+        return "This will be a list of commands that will be set later";
+    }
+    
+    public static String getGrantsUsername(String grantString){
+        String substring = grantString.substring(11,grantString.length());
+        return substring;
+    }
+    
+    
+    public static void showAllCommands(){
+        System.out.println("SHOWING ALL THE COMMANDS");
+    }
+    
+    public static boolean createViewCheck(ArrayList<String> listCol, ArrayList<String> aliasCol){
+        //if the list are the same size, then it is correct
+        if(listCol.size() == aliasCol.size()){
+            return true;
+        }
+        return false;
+    }
+    
+    public static int returnRoleID(String rolename){
+        
+       
+      
+        if(rolename.equals("WAREHOUSE")){
+            
+            return 100;
+        } else if(rolename.equals("SALESPERSON")){
+            return 101;
+        }
+        return -1;
+    }
+    
+    public static int getRoleKey(String rolename){
+        if(rolename.equals("WAREHOUSE")){
+            return 101;
+        }else if(rolename.equals("SALESPERSON")){
+            return 302;
+        }
+        return -1;
+    }
+    public static boolean validRole(String rolename){
+        String formated = rolename.toUpperCase();
+        for(int i = 0; i < roleArray.length;i++){
+            if(roleArray[i].equals(formated)){
+                return true;
+            }
+        }
+        return false;
+    }
+    /*
+    public static String getViewName(String input){
+        String format = input.toUpperCase();
+        
+        if(format.equals("GERMANY")){
+            return "Germany_View";
+        }else if(format.equals("NEW YORK")){
+            return "New_York_View";
+        } else if(format.equals("VANCOUVER")){
+            return "Vancouver_View";
+        }
+        return "ERORR";
+    }
+*/
+}
---
+++ finalJar/Session.java
@@ -0,0 +1,109 @@
+//package client;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.Scanner;
+
+import javax.swing.*;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class Session extends JFrame implements ListSelectionListener, ActionListener {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private JList list;
+    private DefaultListModel chatRoomList;
+    private JLabel lbUserInfo;
+    private JButton btnJoin;
+    private JButton btnAccess;
+    private JButton btnLogOut;
+    private String username;
+    private final int CHATROOM_PORT = 8081;
+
+    
+    public Session(String username) {
+        this.username = username;
+        JPanel userInfoPn = new JPanel();
+        lbUserInfo = new JLabel("Logged in as " + username);
+        btnLogOut = new JButton("Sign Out");
+        btnLogOut.addActionListener(this);
+        userInfoPn.setLayout(new BorderLayout());
+        userInfoPn.add(lbUserInfo, BorderLayout.LINE_START);
+        userInfoPn.add(btnLogOut, BorderLayout.LINE_END);
+        chatRoomList = new DefaultListModel();
+        for (int i = 8081; i < 8091; i++) {
+            chatRoomList.addElement("Chatroom port: " + i);
+        }
+        list = new JList(chatRoomList);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setSelectedIndex(0);
+        list.addListSelectionListener(this);
+        list.setVisibleRowCount(5);
+        JScrollPane listScrollPane = new JScrollPane(list);
+
+        JPanel btnPn = new JPanel();
+        btnJoin = new JButton("Join selected Chatroom");
+        btnJoin.addActionListener(this);
+        btnAccess = new JButton("Access File System");
+        btnAccess.addActionListener(this);
+        btnPn.setLayout(new BorderLayout());
+        btnPn.add(btnJoin, BorderLayout.WEST);
+        btnPn.add(btnAccess, BorderLayout.EAST);
+
+        setLayout(new BorderLayout());
+        add(userInfoPn, BorderLayout.PAGE_START);
+        add(listScrollPane, BorderLayout.CENTER);
+        add(btnPn, BorderLayout.PAGE_END);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 700);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        if (e.getValueIsAdjusting() == false) {
+ 
+            if (list.getSelectedIndex() == -1) {
+            //No selection, disable join button.
+                btnJoin.setEnabled(false);
+ 
+            } else {
+            //Selection, enable the fire button.
+                btnJoin.setEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnJoin){
+            int index = list.getSelectedIndex();
+            int size = chatRoomList.getSize();
+            if (size == 0) { // There is no chatroom, disable the join button.
+                btnJoin.setEnabled(false);
+            } else { //Select an index.
+                list.setSelectedIndex(index);
+                list.ensureIndexIsVisible(index);
+                new ChatRoom(index + CHATROOM_PORT, username);
+            }
+        } else if (e.getSource() == btnAccess) {
+            new ABCFileSystem();
+        } else if (e.getSource() == btnLogOut) {
+            System.exit(0);
+        }
+    }
+}
